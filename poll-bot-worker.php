f5c5570622c935691986b9d62d9a23e9686cde1a
filhoronaldo <email@ronaldofilho.com>
2016-05-22 00:39:29 -0300

    Update poll-bot-worker.php

--- poll-bot-worker.php
+++ poll-bot-worker.php
@@ -6,7 +6,7 @@ set_time_limit(0);
 
 require_once 'PollBot.php';
 
-define('BOT_TOKEN', 'XXXXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '203130442:AAGpk6XhWVgXrPZf0L3wxQVPawkat-h-XaU');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
 $bot->runLongpoll();
